49b9f8548900d8b36da1ae2066857da1a6641c74
luisssqg <ldquintero2004@hotmail.com>
2025-02-25 14:19:54 -0600

    add_folders_&_add_files

---
+++ main.py
@@ -0,0 +1,6 @@
+from Neural_Network_Quinterito.src.sigmoid import sigmoid_fuction
+
+def main():
+
+    return 0
+
---
+++ src/sigmoid.py
@@ -0,0 +1,8 @@
+"""
+    Programa for ...
+"""
+import numpy as np
+import matplotlib as plt
+
+def sigmoid_fuction():
+    return 0
